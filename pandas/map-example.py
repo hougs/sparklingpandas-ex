3bed69219d21e13f5317713e5e5e64a30e529701
Holden Karau <holden@pigscanfly.ca>
2014-08-07 17:16:42 -0700

    Add the map-example in native pandas and pyspark

---
+++ pandas/map-example.py
@@ -0,0 +1,12 @@
+"""Illustrates loading a simple dataframe, distributing it, and then mapping over it."""
+
+from pyspark import SparkContext
+
+sc = SparkContext(appName="PandaMapExample")
+input = [[1, 2], [1, 2], [3, 4]]
+rdd = sc.parallelize(zip(range(len(input)), input))
+frames = rdd.map(lambda x: pd.DataFrame(data = [x[1]], index = [x[0]]))
+sqF = frames.map(lambda f: f.applymap(lambda x: x * x))
+sqDFs = sqF.collect()
+sqDF = reduce(lambda x,y: x.append(y), sqDFs)
+
